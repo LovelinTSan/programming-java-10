18f484d1ad5abffd0bef27baaf7eaf72ae83bbd6
LovelinTSan <148951290+LovelinTSan@users.noreply.github.com>
2023-10-28 17:15:02 +0800

    HelloWorldApp.java

--- tasksheet-1-1-1.java
+++ tasksheet-1-1-1.java
@@ -1,9 +1,6 @@
-/** 
- * The HelloWorldApp class implements an application that
- * simply prints "Hello World!" to standard output.
- */
-class HelloWorldApp {
-    public static void main(String[] args) {
-        System.out.println("Kamusta Mundo!"); // Display the string.
+public class HelloWorldApp {
+    public static void main (String [] args)
+    {
+        System.out.println("Kamusta Mundo");
     }
 }
